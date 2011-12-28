2b3a9847a454b273b8179153de446942fdd9ace3
Dave Albert <dave.albert@gmail.com>
2011-12-28 09:41:13 +0000

    Added Mindfulness tab.

---
+++ PublicZen/PublicZen/MindfulnessViewController.m
@@ -0,0 +1,57 @@
+//
+//  MindfulnessViewController.m
+//  PublicZen
+//
+//  Created by Dave Albert on 28/12/2011.
+//  Copyright 2011 Hermitage Medical Clinic. All rights reserved.
+//
+
+#import "MindfulnessViewController.h"
+
+
+@implementation MindfulnessViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
